bf6817a6ec8cd0e4dde403436f6497687c3f0296
v-petrenko <petrenko.v@bk.ru>
2019-08-19 07:34:19 +0300

    Удалена дублирующаяя строка с параметрами. и print (Вам предстоит много работы)

--- Profile.py
+++ Profile.py
@@ -1,9 +1,13 @@
 print('МЕДЕЦИНСКАЯ АНКЕТА')
-petreson_name=(input('Введите свое имя: '))
-family=(input('Введите свою фамилию: '))
-age=(input('Введите свой возраст: '))
-weight=(input('Введите свой вес: '))
-print(petreson_name,family,',',age,'год',',','вес', weight,'- молодец, так держать!')
-print(petreson_name,family,',',age,'год',',','вес', weight,'- следует заняться йогой.')
-print(petreson_name,family,',',age,'год',',','вес', weight,'- необходимо обратиться к врачу!')
-print(petreson_name,family,',',age,'год',',','вес', weight,'- Вам предстоит мого работы!')
+petreson_name = (input('Введите свое имя: '))
+family = (input('Введите свою фамилию: '))
+age = int(input('Ваш возраст: '))
+weight = int(input('Сколько Вы весите: '))
+if age <= 30 and weight >= 120 and weight <= 50 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- следует заняться йогой.')
+elif age >= 40 and weight >= 120 and weight <= 50 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- необходимо обратиться к врачу!')
+elif age <= 30 and weight >= 50 and weight <= 120 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- молодец, так держать!')
+else:
+    print('с вводными что-то не так')
