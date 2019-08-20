55f508718968d7708d12d39d78bf1296043a1dbe
v-petrenko <petrenko.v@bk.ru>
2019-08-20 23:47:35 +0300

    Profile v.2.0

--- Profile.py
+++ Profile.py
@@ -3,11 +3,17 @@ petreson_name = (input('Введите свое имя: '))
 family = (input('Введите свою фамилию: '))
 age = int(input('Ваш возраст: '))
 weight = int(input('Сколько Вы весите: '))
-if age <= 30 and weight >= 120 and weight <= 50 :
-    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- следует заняться йогой.')
-elif age >= 40 and weight >= 120 and weight <= 50 :
-    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- необходимо обратиться к врачу!')
-elif age <= 30 and weight >= 50 and weight <= 120 :
+if age <= 30 and 50 <= weight <= 120 :
     print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- молодец, так держать!')
-else:
+elif age <= 30 and weight >= 120 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- необходимо обратиться к врачу!')
+elif 30 <= age <= 39 and weight <= 50 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- следует заняться йогой.')
+elif 30 <= age <= 39 and weight >= 120 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- следует заняться йогой.')
+elif age >= 40 and weight <= 50 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- необходимо обратиться к врачу!')
+elif age >= 40 and weight >= 120 :
+    print(petreson_name, family, ',', age, 'год', ',', 'вес', weight, '- необходимо обратиться к врачу!')
+else :
     print('с вводными что-то не так')
